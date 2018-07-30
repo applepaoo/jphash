f03d8aa9af91fff5724d5c273579769443347c29
applepaoo <applepaoo@gmail.com>
2018-07-30 16:44:21 +0800

    合併成單一json

--- src/main/java/com/pragone/jphash/jpHash.java
+++ src/main/java/com/pragone/jphash/jpHash.java
@@ -4,7 +4,6 @@ import com.pragone.jphash.image.radial.RadialHash;
 import com.pragone.jphash.image.radial.RadialHashAlgorithm;
 import com.sun.jmx.snmp.SnmpUnknownSubSystemException;
 import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +12,8 @@ import java.math.BigInteger;
 import java.text.Normalizer;
 import java.text.SimpleDateFormat;
 import java.util.*;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import org.omg.CORBA.PUBLIC_MEMBER;
@@ -41,6 +42,7 @@ public class jpHash {
 		return RadialHashAlgorithm.getSimilarity(hash1, hash2);
 	}
 
+	// 16進位轉2進位
 	public static String hexToBinary(String hexnumber) {
 		BigInteger temp = new BigInteger(hexnumber, 16);
 		return temp.toString(2);
@@ -58,9 +60,10 @@ public class jpHash {
 		RadialHash hash1 = jpHash.getImageRadialHash("airpods/airpods1.jpg");
 		String Hash1 = hash1.toString();
 
-		System.out.println("hash: " + hash1);
-		System.out.println("fingerPrint: " + hexToBinary(Hash1));
-		System.out.println("---------------------------------");
+		// 原圖hash
+		// System.out.println("hash: " + hash1);
+		// System.out.println("fingerPrint: " + hexToBinary(Hash1));
+		// System.out.println("---------------------------------");
 
 		// FigureN
 		File file = new File("D:\\workspace2\\jphash\\airpods");
@@ -69,35 +72,43 @@ public class jpHash {
 		if (file.isDirectory()) {
 
 			filenames = file.list();
+			ArrayList dataList = new ArrayList();
 
 			for (int i = 0; i < filenames.length; i++)
 
 			{
 
 				RadialHash hash2 = jpHash.getImageRadialHash(file + "/" + filenames[i]);
-				String Hash2 = hash2.toString();
-				System.out.println(filenames[i]);
-				System.out.println("hash: " + hash2);
-				System.out.println("fingerPrint: " + hexToBinary(Hash2));
+				String Hash2 = hash2.toString();// 算出的Fingerprint
+
+				// System.out.println(filenames[i]);
+				// System.out.println("hash: " + hash2);
+				// System.out.println("fingerPrint: " + hexToBinary(Hash2));
 
 				BigInteger i1 = new BigInteger(Hash1, 16);
 				BigInteger i2 = new BigInteger(Hash2, 16);
 				int distance = hammingDistance(i1, i2);
+
 				// System.out.println("Hamming Distance: " + distance);
 				// System.out.println("Similarity: " + jpHash.getSimilarity(hash1, hash2));
 
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'"); // 格式化時間
 				Date current = new Date();
-				System.out.println(sdf.format(current));
+				// System.out.println(sdf.format(current));
 
 				JSONObject obj = new JSONObject();
+
 				obj.put("G_NO", filenames[i]);
 				obj.put("G_FINGERPRINT", hexToBinary(Hash2));
 				obj.put("_SOURCE_TIME", sdf.format(current));
-				System.out.println(obj);
-				System.out.println("--------------------------------");
+				dataList.add(i, obj);
+
+				// System.out.println(obj);
+				// System.out.println("--------------------------------");
 
 			}
+
+			System.out.println(dataList);
 
 		}
 
